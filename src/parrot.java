bf1b65216a8b795fbce4e7431ce090551dc727d3
77impulse77 <scugorshkof@gmail.com>
2022-05-04 20:54:58 +0300

    хы!

--- src/parrot.java
+++
@@ -1,8 +0,0 @@
-public class parrot extends  bird {
-    parrot(String name, String color, int age, int height, int obstacles) {
-        super(name, color, age, height, obstacles);
-    }
-    void  speak() {
-        System.out.println(name + " хочет рыбы!" );
-    }
-}
